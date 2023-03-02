3df286e747851895dc73178b6e823c52d9eec2f3
Vadim Cherepanov <xenos26@gmail.com>
2023-03-02 23:40:39 +0700

    add Work page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d1cd7d6b4f3f5fd6"; }
+/******/ 		__webpack_require__.h = function() { return "c1159c76a287c30c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/works.js
+++ pages/works.js
@@ -1,5 +1,9 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item.js'
+
+import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
+import thumbWalknote from '../public/images/works/walknote_eyecatch.png'
 
 const Works = () => {
   return (
@@ -7,6 +11,19 @@ const Works = () => {
       <Heading as="h3" fontSize={20} mb={4}>
         Works
       </Heading>
+
+      <SimpleGrid columns={[1,1,2]} gap={6}>
+    <Section>
+      <WorkGridItem id="inkdrop" title="Inkdrop" thumbnail={thumbInkdrop}>
+    A markdown note taking app
+    </WorkGridItem>
+    </Section>
+    <Section>
+    <WorkGridItem id="walknote" title="walknote" thumbnail={thumbWalknote} >
+    Music recomendation app for IOS
+    </WorkGridItem>
+    </Section>
+    </SimpleGrid>
     </Container>
   )
 }
