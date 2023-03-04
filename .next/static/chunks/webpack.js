c2267390dc5b288cb0817b68d6ab136bb675a7a2
Vadim Cherepanov <xenos26@gmail.com>
2023-03-05 00:21:59 +0700

    add 404 page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ce6dd3ba3c9007f1"; }
+/******/ 		__webpack_require__.h = function() { return "5ac8747f44e8e484"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
