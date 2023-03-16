0ac13ef225063cfac44f744114f61136ad34f94d
Vadim Cherepanov <xenos26@gmail.com>
2023-03-17 01:24:58 +0700

    fix: fix double model bug

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0d876153de23c488"; }
+/******/ 		__webpack_require__.h = function() { return "900fcaa907822686"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/voxel-computer.js
+++ components/voxel-computer.js
@@ -1,9 +1,8 @@
 import { useState, useEffect, useCallback, useRef } from 'react'
-import { Box, Spinner } from '@chakra-ui/react'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { loadGLTFModel } from '../lib/model'
-import { Matrix4 } from 'three'
+import { ComputerContainer, ComputerSpinner } from './voxel-computer-loader'
 
 function easeOutCirc(x) {
   return Math.sqrt(1 - Math.pow(x - 1, 4))
@@ -12,20 +11,11 @@ function easeOutCirc(x) {
 const VoxelComputer = () => {
   const refContainer = useRef()
   const [loading, setLoading] = useState(true)
-  const [renderer, setRenderer] = useState()
-  const [_camera, setCamera] = useState()
-  const [target] = useState(new THREE.Vector3(-0.5, 1.2, 0))
-  const [initialCameraPosition] = useState(
-    new THREE.Vector3(
-      20 * Math.sin(0.2 * Math.PI),
-      10,
-      20 * Math.cos(0.2 * Math.PI)
-    )
-  )
-  const [scene] = useState(new THREE.Scene())
-  const [_controls, setControls] = useState()
+  const refRenderer = useRef()
+  const urlDogGLB = (process.env.NODE_ENV === 'production' ? 'https://craftzdog.global.ssl.fastly.net/homepage' : '') + '/computer.glb'
 
   const handleWindowResize = useCallback(() => {
+    const { current: renderer } = refRenderer
     const { current: container } = refContainer
     if (container && renderer) {
       const scW = container.clientWidth
@@ -33,12 +23,12 @@ const VoxelComputer = () => {
 
       renderer.setSize(scW, scH)
     }
-  })
+  }, [])
 
   /* eslint-disable react-hooks/exhaustive-deps */
   useEffect(() => {
     const { current: container } = refContainer
-    if (container && !renderer) {
+    if (container) {
       const scW = container.clientWidth
       const scH = container.clientHeight
 
@@ -50,10 +40,18 @@ const VoxelComputer = () => {
       renderer.setSize(scW, scH)
       renderer.outputEncoding = THREE.sRGBEncoding
       container.appendChild(renderer.domElement)
-      setRenderer(renderer)
+      refRenderer.current = renderer
+      const scene = new THREE.Scene()
+
+      const target = new THREE.Vector3(-0.5, 1.2, 0)
+      const initialCameraPosition = new THREE.Vector3(
+        20 * Math.sin(0.2 * Math.PI),
+        10,
+        20 * Math.cos(0.2 * Math.PI)
+      )
 
       // 640 -> 240
-      // 8 -> 6
+      // 8   -> 6
       const scale = scH * 0.005 + 4.8
       const camera = new THREE.OrthographicCamera(
         -scale,
@@ -65,7 +63,6 @@ const VoxelComputer = () => {
       )
       camera.position.copy(initialCameraPosition)
       camera.lookAt(target)
-      setCamera(camera)
 
       const ambientLight = new THREE.AmbientLight(0xcccccc, 1)
       scene.add(ambientLight)
@@ -73,9 +70,8 @@ const VoxelComputer = () => {
       const controls = new OrbitControls(camera, renderer.domElement)
       controls.autoRotate = true
       controls.target = target
-      setControls(controls)
 
-      loadGLTFModel(scene, '/computer.glb', {
+      loadGLTFModel(scene, urlDogGLB, {
         receiveShadow: false,
         castShadow: false
       }).then(() => {
@@ -109,6 +105,7 @@ const VoxelComputer = () => {
 
       return () => {
         cancelAnimationFrame(req)
+        renderer.domElement.remove()
         renderer.dispose()
       }
     }
@@ -119,30 +116,10 @@ const VoxelComputer = () => {
     return () => {
       window.removeEventListener('resize', handleWindowResize, false)
     }
-  }, [renderer, handleWindowResize])
+  }, [handleWindowResize])
 
   return (
-    <Box
-      ref={refContainer}
-      className="voxel-computer"
-      m="auto"
-      mt={['-20px', '-60px', '-120px']}
-      mb={['-40px', '-140px', '-200px']}
-      w={[280, 480, 640]}
-      h={[280, 480, 640]}
-      position="relative"
-    >
-      {loading && (
-        <Spinner
-          size="xl"
-          position="absolute"
-          left="50%"
-          top="50%"
-          ml="calc(0px - var(--spinner-size) / 2)"
-          mt="calc(0px - var(--spinner-size))"
-        />
-      )}
-    </Box>
+    <ComputerContainer ref={refContainer}>{loading && <ComputerSpinner />}</ComputerContainer>
   )
 }
 
